441f2986a9fb0a123e2e05067c4f0900478c7c40
JoseanG24 <joseandresg444@gmail.com>
2024-10-01 17:37:14 -0600

    third commit, about section, layout, metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import HeroSection from "@/components/home/Hero"
-import Head from "next/head"
+import HeroSection from "@/components/home/Hero";
+import AboutSection from "@/components/home/About";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <>
       <Head>
-        <title>Clínica Punto Dental - Tu Dentista en Carretera a El Salvador, Guatemala</title>
+        <title>
+          Clínica Punto Dental - Tu Dentista en Carretera a El Salvador,
+          Guatemala
+        </title>
         <meta
           name="description"
           content="Clínica dental en Carretera a El Salvador que ofrece servicios de odontología general, ortodoncia, implantes dentales y estética dental en Guatemala."
@@ -14,20 +18,29 @@ const HomePage = () => {
           name="keywords"
           content="Dentista Carretera a El Salvador, Clínica dental Guatemala, Odontología, Ortodoncia, Implantes Dentales"
         />
-        <meta property="og:title" content="Clínica Punto Dental - Dentista en Carretera a El Salvador" />
+        <meta
+          property="og:title"
+          content="Clínica Punto Dental - Dentista en Carretera a El Salvador"
+        />
         <meta
           property="og:description"
           content="Ofrecemos servicios dentales como odontología general, ortodoncia, estética dental e implantes en Carretera a El Salvador, Guatemala."
         />
         <meta property="og:url" content="https://tudominio.com" />
-        <meta property="og:image" content="https://tudominio.com/imagen-clinica.jpg" />
+        <meta
+          property="og:image"
+          content="https://tudominio.com/imagen-clinica.jpg"
+        />
       </Head>
 
       <div>
         <HeroSection />
+        <div id="nosotros">
+          <AboutSection />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ src/components/home/About.jsx
@@ -0,0 +1,172 @@
+"use client";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+
+// Registra ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+const AboutSection = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      ".about-title",
+      { opacity: 0, x: -50 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".about-title",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".about-description",
+      { opacity: 0, x: 50 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".about-description",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".about-info",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.4,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".about-info",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".about-image",
+      { opacity: 0, scale: 0.9 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 1.6,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".about-image",
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <section className="relative bg-gray-100 text-[#05192e] py-20">
+      <div className="container mx-auto flex flex-col md:flex-row items-start justify-between px-4 md:px-0">
+        {/* Mapa para pantallas grandes */}
+        <div className="hidden md:block w-full md:ml-20 md:w-1/2 md:sticky md:top-32 h-96 mb-12">
+          <h2 className="font-bold">Ubicación</h2>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d482.7927445547332!2d-90.47129676508176!3d14.522422300000017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a4e0e5d1670f%3A0x4bf8387a674eee34!2sGGCH%2BXJ6%2C%20Unnamed%20Road%2C%20San%20Jorge%20Muxbal!5e0!3m2!1ses-419!2sgt!4v1725574780010!5m2!1ses-419!2sgt"
+            className="w-full h-full"
+            style={{ border: 0 }}
+            // allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+
+        {/* Mapa optimizado para mobile */}
+        <div className="block md:hidden w-full h-64 mb-12">
+          <h2 className="font-bold">Ubicación</h2>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d482.7927445547332!2d-90.47129676508176!3d14.522422300000017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a4e0e5d1670f%3A0x4bf8387a674eee34!2sGGCH%2BXJ6%2C%20Unnamed%20Road%2C%20San%20Jorge%20Muxbal!5e0!3m2!1ses-419!2sgt!4v1725574780010!5m2!1ses-419!2sgt"
+            className="w-full h-full"
+            style={{ border: 0 }}
+            // allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+
+        {/* Información de la Clínica */}
+        <div className="w-full md:w-1/2 mt-10 md:mt-0 md:pl-10 md:pr-7 bg-[#f3f3f3] py-20">
+          <h2 className="about-title text-3xl md:text-4xl font-bold mb-4">
+            Conócenos
+          </h2>
+          {/* Imagen de la Clínica o del Equipo */}
+          <div className="mb-10 mt-1 w-full">
+            <Image
+              src="/images/imgclinica1.png"
+              alt="Clínica Punto Dental"
+              width={900}
+              height={900}
+              className="about-image rounded-lg shadow-lg w-full"
+            />
+          </div>
+          <p className="about-description text-base md:text-lg mb-6">
+            Somos una clínica dental dedicada a ofrecer un cuidado dental de la
+            más alta calidad con un enfoque en la comodidad y satisfacción de
+            nuestros pacientes. Nuestra misión es transformar sonrisas y mejorar
+            la calidad de vida de nuestros pacientes a través de servicios
+            dentales excepcionales.
+          </p>
+
+          {/* Información de Contacto, Servicios y Dirección */}
+          <div className="about-info space-y-4">
+            <h3 className="text-xl md:text-4xl font-semibold text-blue-900">
+              Nuestros Servicios
+            </h3>
+            <ul className="list-disc list-inside mb-4 text-base md:text-lg">
+              <li>Odontología General</li>
+              <li>Ortodoncia</li>
+              <li>Implantes Dentales</li>
+              <li>Estética Dental</li>
+              <li>Periodoncia</li>
+            </ul>
+
+            <h3 className="text-xl md:text-4xl font-semibold text-blue-900">
+              Contacto
+            </h3>
+            <p className="mb-2 text-base md:text-lg">
+              <strong>Teléfono:</strong> +1 123-456-7890
+            </p>
+            <p className="mb-2 text-base md:text-lg">
+              <strong>Email:</strong> contacto@puntdental.com
+            </p>
+
+            <h3 className="text-xl md:text-4xl font-semibold text-blue-900">
+              Dirección
+            </h3>
+            <p className="mb-2 text-base md:text-lg">
+              123 Calle Principal, Ciudad Dental, País.
+            </p>
+
+            <h3 className="text-xl md:text-4xl font-semibold text-blue-900">
+              Horario de Atención
+            </h3>
+            <p className="text-base md:text-lg">
+              Lunes a Viernes: 9:00 AM - 6:00 PM
+            </p>
+            <p className="text-base md:text-lg">Sábado: 9:00 AM - 2:00 PM</p>
+            <p className="text-base md:text-lg">Domingo: Cerrado</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
